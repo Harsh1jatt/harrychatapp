84822ed799403014eed13434e3e38215edeab290
Harsh1jatt <148642923+Harsh1jatt@users.noreply.github.com>
2024-08-06 18:12:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,6 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
-server.listen(3000, () => {
+server.listen(3860, () => {
     console.log('Server is running on port 3000');
 });
